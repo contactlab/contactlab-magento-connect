239bc71af08cb31a6d2c1d050f51dc611627e1e4
andreagaspardo <andrea.gaspardo@webformat.com>
2015-09-11 12:25:23 +0200

    Updated travis yml

---
+++ app/code/community/Contactlab/Commons/Test/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+
+class Contactlab_Commons_Test_Helper_Data extends EcomDev_PHPUnit_Test_Case
+{
+    /**
+     * @test
+     *
+     * @return bool
+     */
+    public function isDebug() {
+        $isDebug = Mage::getStoreConfigFlag("contactlab_commons/global/debug");
+        $this->assertFalse($isDebug);
+    }
+}
