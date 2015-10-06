1916fe02f891799a52c24935917e02589b330db7
Paolo Santori <code@paolosantori.it>
2015-10-06 20:30:54 +0200

    Modified build script so as to take no-more-ignored files into account

--- build.sh
+++ build.sh
@@ -16,9 +16,12 @@ plugin="${p_in}/lib/contactlab \
         ${p_in}/app/code/community/Contactlab \
         ${p_in}/app/locale/en_US/contactlab \
         ${p_in}/app/locale/en_US/template/email/contactlab_commons \
+        ${p_in}/app/locale/en_US/template/email/contactlab_subscribers \
         ${p_in}/app/locale/it_IT/contactlab \
         ${p_in}/app/design/adminhtml/default/default/layout/contactlab \
         ${p_in}/app/design/adminhtml/default/default/template/contactlab \
+        ${p_in}/app/design/frontend/base/default/layout/contactlab \
+        ${p_in}/app/design/frontend/base/default/template/contactlab \
         ${p_in}/js/contactlab_commons ${p_in}/skin/adminhtml/default/default/images/contactlab"
 
 tar -cvzf ${p_out}/contactlab-magento-step1-plugin.tgz $plugin
