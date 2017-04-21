116d2936c9c637d7b60ee34086417c3c7b2f5766
fdeluciatunda <f.delucia@tunda.com>
2017-04-21 10:22:32 +0200

    add check email, add check SftpSubscriberExporter and SftpXmlDelivery

--- app/code/community/Contactlab/Commons/controllers/Adminhtml/Contactlab/Commons/ConfigurationCheckController.php
+++ app/code/community/Contactlab/Commons/controllers/Adminhtml/Contactlab/Commons/ConfigurationCheckController.php
@@ -22,4 +22,32 @@ class Contactlab_Commons_Adminhtml_Contactlab_Commons_ConfigurationCheckControll
     {
         return Mage::getSingleton('admin/session')->isAllowed('newsletter/contactlab/configuration_check');
     }
+    
+    public function testemailAction(){      	
+    	$toEmail = $this->getRequest()->getParam('mail');    	
+    	if($toEmail)
+    	{    	
+    		$toName = $toEmail;
+	    	$fromEmail = Mage::getStoreConfig('trans_email/ident_general/email');
+	    	$fromName = Mage::getStoreConfig('trans_email/ident_general/name');
+	    	
+	    	$body = Mage::helper('contactlab_commons')->__('This is a Contactlab Test Email!');
+	    	$subject = Mage::helper('contactlab_commons')->__('Contactlab Test Email');
+
+	    	$mail = Mage::getModel('contactlab_transactional/zend_mail', 'utf-8');
+	    	$mail->setBodyText($body);
+	    	$mail->setFrom($fromEmail, $fromName);
+	    	$mail->addTo($toEmail, $toName);
+	    	$mail->setSubject($subject);
+	    	try {
+	    		$mail->send();	    		
+	    		Mage::getSingleton('core/session')->addSuccess(Mage::helper('contactlab_commons')->__('Test email sent seccesfully.'));
+	    	}
+	    	catch(Exception $ex) {	    		
+	    		Mage::getSingleton('core/session')->addError(Mage::helper('contactlab_commons')->__('Unable to send test email.'));
+	    	}
+	    
+    	}
+    	$this->_redirect('*/*/index');
+    }
 }
